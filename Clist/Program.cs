ade604b43a123089b704a61b42f223ccb8a96d6b
Beatriz Trindade <beatriztrindade@hotmail.com>
2022-07-25 18:12:13 -0300

    Ex01_Lista

---
+++ Clist/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("-----------------");
+            Console.WriteLine("- Animais List! -");
+            Console.WriteLine("-----------------");
+
+            List<string> lista = new List<string>();
+
+            lista.Add("Cachorro");
+            lista.Add("Aranha");
+            lista.Add("Rato");
+            lista.Add("Mosca");
+
+            lista.Insert(2,"Gato");
+
+            lista.Remove("Mosca");
+            lista.RemoveAt(1);
+
+            foreach (string nome in lista)
+            {
+                Console.WriteLine($"Animal: {nome} \n");
+            }
+        }
+    }
+}
